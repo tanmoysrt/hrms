4e505f8bcf658ba58964fd3b6e2e00dbf29f9286
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-12-26 16:04:16 +0530

    chore: remove local var

--- hrms/__init__.py
+++ hrms/__init__.py
@@ -3,4 +3,3 @@ __version__ = "16.0.0-dev"
 from hrms.frappe_notification import FrappeNotification
 
 FrappeNotification.set_project("hrms")
-FrappeNotification.CENTRAL_SERVER_ENDPOINT = "http://notification.relay:8000"
