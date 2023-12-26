1c3162b83c132cc34b1dee633f4af9591ccaa7d8
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-12-26 16:29:45 +0530

    chore: fix bug

--- frontend/public/frappe-notification-sw.js
+++ frontend/public/frappe-notification-sw.js
@@ -1,23 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-sw.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {getMessaging, onBackgroundMessage} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-sw.js";
 
 const jsonConfig = new URL(location).searchParams.get('config');
 const firebaseApp = initializeApp(JSON.parse(jsonConfig));
 const messaging = getMessaging(firebaseApp);
 
 onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.data.title;
-    const notificationOptions = {
-        body: payload.data.body || '',
-        data: { url:payload.data.click_action }
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
+	console.log('[firebase-messaging-sw.js] Received background message ', payload);
+	const notificationTitle = payload.data.title;
+	const notificationOptions = {
+		body: payload.data.body || '',
+		data: {url: payload.data.click_action}
+	};
+	self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener('notificationclick', (event)=>{
-  event.notification.close();
-  if(!event.notification.data.url){
-        clients.openWindow(event.notification.data.url)
-  };
+self.addEventListener('notificationclick', (event) => {
+	event.stopImmediatePropagation();
+	event.notification.close();
+	if (event.notification.data.url) {
+		clients.openWindow(event.notification.data.url)
+	}
+	;
 })
