3b034f748df41132ee5d927ba433298ba384a29e
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-12-27 16:11:52 +0530

    feat: disable link click support except chrome

--- frontend/public/frappe-notification-sw.js
+++ frontend/public/frappe-notification-sw.js
@@ -5,21 +5,31 @@ const jsonConfig = new URL(location).searchParams.get('config');
 const firebaseApp = initializeApp(JSON.parse(jsonConfig));
 const messaging = getMessaging(firebaseApp);
 
+function isChrome() {
+	return navigator.userAgent.toLowerCase().includes("chrome")
+}
+
 onBackgroundMessage(messaging, (payload) => {
 	console.log('[firebase-messaging-sw.js] Received background message ', payload);
 	const notificationTitle = payload.data.title;
-	const notificationOptions = {
-		body: payload.data.body || '',
-		data: {url: payload.data.click_action}
+	let notificationOptions = {
+		body: payload.data.body || ''
 	};
+	if (isChrome()) {
+		notificationOptions['data'] = {
+			url: payload.data.click_action
+		}
+	}
 	self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener('notificationclick', (event) => {
-	event.stopImmediatePropagation();
-	event.notification.close();
-	if (event.notification.data.url) {
-		clients.openWindow(event.notification.data.url)
-	}
-	;
-})
+
+if(isChrome()) {
+	self.addEventListener('notificationclick', (event) => {
+		event.stopImmediatePropagation();
+		event.notification.close();
+		if (event.notification.data.url) {
+			clients.openWindow(event.notification.data.url)
+		}
+	})
+}
