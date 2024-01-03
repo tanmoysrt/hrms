e2062681da98e9512ea501e8b4fd4d89c044bee5
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-03 11:05:19 +0530

    chore: rollback relay url change

--- hrms/__init__.py
+++ hrms/__init__.py
@@ -3,4 +3,3 @@ __version__ = "16.0.0-dev"
 from hrms.frappe_notification import FrappeNotification
 
 FrappeNotification.set_project("hrms")
-FrappeNotification.set_central_server_endpoint("http://notification.relay:8000")
