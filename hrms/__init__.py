203a47120a9f970f0900c82f2dfbd37113540e78
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-13 12:11:02 +0530

    chore: use class variable instead of static variable

--- hrms/__init__.py
+++ hrms/__init__.py
@@ -3,7 +3,6 @@ from frappe.push_notification import PushNotification
 
 __version__ = "16.0.0-dev"
 
-
 def refetch_resource(cache_key: str | list, user=None):
 	frappe.publish_realtime(
 		"hrms:refetch_resource",
@@ -11,6 +10,3 @@ def refetch_resource(cache_key: str | list, user=None):
 		user=user or frappe.session.user,
 		after_commit=True,
 	)
-
-
-PushNotification.set_project("hrms")
